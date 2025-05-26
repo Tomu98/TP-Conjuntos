e1c5e840378b891e72c48653755b89b5bfd2808a
Alx1as <alexiarubin23@gmail.com>
2025-05-26 13:32:42 -0300

    Empezando Operaciones con años de nacimiento

--- TP_Conjuntos.py
+++ TP_Conjuntos.py
@@ -1,3 +1,6 @@
+#Parte 2 – Desarrollo del Programa en Python
+# A. Operaciones con DNIs
+
 # Ingresar dni
 def dni_conjunto():
     while True:
@@ -14,5 +17,44 @@ def dni_conjunto():
             break
 
 dni_conjunto()
+
+
+
+
+
+
+
+# B. Operaciones con años de nacimiento
+#a. Ingreso de los años de nacimiento
+def anio_nacimiento():
+    anios = []
+    #bucle para obtener los 5 inputs
+    for i in range (5):
+        anio= int(input("Ingresá un año de nacimiento: "))
+        anios.append(anio) #Agregamos cada año a la lista
+    print("Años ingresados:", anios)
+    return anios #devuelve la lista con los resultados
+
+anios=anio_nacimiento() #lo que ejecuta la función se guarda en la variable anios 
+
+#b. Imprimir pares e impares 
+pares=0
+impares=0
+for anio in anios:
+    if anio %2==0: pares+=1
+    else: impares+=1
+print(f"Pares: {pares} Impares: {impares}")
+
+#c. Imprimir Grupo Z si todos somos > 2000
+if all(anio>2000 for anio in anios): #se pueden poner bucles dentro de condicionales :O. All asegura que todos los elementos de la lista sean > 2000
+    print("Grupo Z")
+
+#d. Año bisiestro = tiene que ser divisible por 4 y NO ser divisible por 100, salvo que también sea divisible por 400. 
+def bisiestro(anios):
+    for anio in anios:
+        if(anio%4==0 and anio%100!=0 or  anio%400==0): #si es divisible por 400 también lo es por 4 y por 100.
+            print("Tenemos un año especial")
+
+
         
         
